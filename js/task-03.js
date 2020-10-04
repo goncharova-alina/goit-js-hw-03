4c8a0d8f691628f0c0e14e550c668809a0562288
Alina Goncharova <goncarovaalina206@gmail.com>
2020-10-04 18:07:17 +0300

    task-03

--- js/task-03.js
+++ js/task-03.js
@@ -1,10 +1,43 @@
-const findBestEmployee = function (employees) {
-    'use strict';
-    // Write code under this line
-    
-  };
+// const findBestEmployee = function (employees) {
+//     'use strict';
+   
+//     let keys = Object.keys(employees);
+//     // console.log(keys.length);
+//     let max = Math.max(...Object.values(employees));
+
+//     for (const key of keys ) {
+//       let value = employees[key];
+//       let values = 0;
+      
+//       if (value === max){
+//         values = value;
+//         return key;  
+//       } if ( keys.length === 0 ) {
+//         let message = ' ';
+//         return message;
+//       }          
+// } 
   
+const findBestEmployee = function (employees) {
+  'use strict';
+  // Write code under this line
+  let bestEmployee = '';
+  const keys = Object.keys(employees);
+   
+  const max = Math.max(...Object.values(employees));
+    for (const key of keys ) {
+      let value = employees[key];
+      if (value === max){
+        bestEmployee = key; 
+      }    
+} 
+  return bestEmployee;  
+};
+
+
   // Объекты и ожидаемый результат
+  const dev = {};
+  console.log(findBestEmployee(dev)); 
   const developers = {
     ann: 29,
     david: 35,
@@ -30,3 +63,12 @@ const findBestEmployee = function (employees) {
   }
   console.log(findBestEmployee(sellers)); 
   // 'lux' 
+
+
+
+// Поиск наибольшего значения среди свойств объекта
+// Напиши функцию findBestEmployee(employees), 
+// которая принимает объект сотрудников 
+// и возвращает имя самого продуктивного (который выполнил больше всех задач). 
+// Сотрудники и кол-во выполненных задач содержатся 
+// как свойства объекта в формате "имя":"кол-во задач".
